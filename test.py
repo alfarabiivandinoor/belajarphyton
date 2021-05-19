044cb3f04c9db4d9e9a5a1fc4358cc367fd26dd6
Alfarabi Ivandi Noor <alfarabiin12@gmail.com>
2021-05-19 21:32:55 +0700

    update

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-# Membuat segitiga dengan perataan kiri-bawah
-T = int(input("Masukan Angka : "))
-for i in range (1, T+1):
-	print(((T-i+1) * " ") + (i * "*"))
+# # Membuat segitiga dengan perataan kiri-bawah
+# T = int(input("Masukan Angka : "))
+# for i in range (1, T+1):
+# 	print(i * "*")
+
+# # Membuat segitiga dengan perataan kiri-atas
+# T = int(input("Masukan Angka : "))
+# for i in range (1, T+1):
+# 	print((T-i+1) * "*")
+
+# Membuat segitiga dengan perataan kanan-bawah
+# T = int(input("Masukan Angka : "))
+# for i in range (1, T+1):
+# 	print(((T-i+1) * " ") + (i * "*"))
