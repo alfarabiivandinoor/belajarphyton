2efc2ed7dcbcf96cc3f2f7723dd5158b00ac5da1
Alfarabi Ivandi Noor <alfarabiin12@gmail.com>
2021-05-19 21:41:59 +0700

    update

--- test.py
+++ test.py
@@ -12,3 +12,8 @@
 # T = int(input("Masukan Angka : "))
 # for i in range (1, T+1):
 # 	print(((T-i+1) * " ") + (i * "*"))
+
+# # Membuat segitga dengan perataan kanan-atas
+# T = int(input("Masukan Angka : "))
+# for i in range (1, T+1):
+# 	print((i * " ") + ((T-i+1) * "*"))
