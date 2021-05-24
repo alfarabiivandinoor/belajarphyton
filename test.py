6478f020b52c4ed6271a276e5f0d2246e762426c
Alfarabi Ivandi Noor <alfarabiin12@gmail.com>
2021-05-24 16:44:15 +0700

    update

--- test.py
+++ test.py
@@ -16,7 +16,7 @@
 # # Membuat segitga siku dengan perataan kanan-atas
 # T = int(input("Masukan Angka : "))
 # for i in range (1, T+1):
-# 	print((i * " ") + ((T-i+1) * "*"))
+# 	print((i * " ") + ((T-i+1) * "*")) 
 
 # # Membuat piramida
 # T = int(input("Masukan Angka : "))
@@ -28,14 +28,13 @@
 # for i in range (1, T+1):
 #     print ((i*" ") + (("*" * (((T-i+1) * 2)-1))))
 
-# Membuat piramida berisi
-T = int(input("Masukan Angka : "))
-# (A, X, B)
-for i in range (1, T+1):
-    # print ((T-i+1) * " " + ("*" * ((i*2)-1)))
-    if i == (((T/2)+1)-1/2):
-        print ((T-i+1) * " " + (int(T/2 - 1/2) * "*") + "o" + (int(T/2 - 1/2) * "*"))
-    elif i == T/2: 
-        print ((T-i+1) * " " + (int(T/2 - 1/2) * "*") + "o" + (int(T/2 - 1/2) * "*"))
-    else :
-        print ((T-i+1) * " " + ("*" * ((i*2)-1)))
+# # Membuat piramida berisi
+# T = int(input("Masukan Angka : "))
+# for i in range (1, T+1):
+#     # print ((T-i+1) * " " + ("*" * ((i*2)-1)))
+#     if i == (((T/2)+1)-1/2):
+#         print ((T-i+1) * " " + (int(T/2 - 1/2) * "*") + "o" + (int(T/2 - 1/2) * "*"))
+#     elif i == T/2: 
+#         print ((T-i+1) * " " + (int(T/2 - 1/2) * "*") + "o" + (int(T/2 - 1/2) * "*"))
+#     else :
+#         print ((T-i+1) * " " + ("*" * ((i*2)-1)))
