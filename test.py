e6f5b33c41190ba6662f3dd1ebf0463c84258815
Alfarabi Ivandi Noor <alfarabiin12@gmail.com>
2021-05-29 00:52:08 +0700

    update

--- test.py
+++ test.py
@@ -118,3 +118,11 @@
 #     z = int(input("Masukan Z : "))
 #     n = int(input("Masukan N : "))
 #     print ([[i,j,k] for i in range (0, x+1) for j in range (0, y+1) for k in range (0, z+1) if i+j+k != n])
+
+# Soal HackerRank $5
+# Return double of n
+if __name__ == '__main__':
+    n = int(input("n : "))
+    arr = (int, input("angka : ").split())
+    if n in arr :
+        print (n)
